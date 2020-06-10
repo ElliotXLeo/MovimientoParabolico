14770fc6ba80d46ab81891d75f58ec4960a8d2df
ElliotXLeo <elliotgaramendi@gmail.com>
2020-06-10 16:38:38 -0500

    interface InterfaceMovimientoParabolico

---
+++ src/clases/InterfaceMovimientoParabolico.java
@@ -0,0 +1,16 @@
+package clases;
+
+public interface InterfaceMovimientoParabolico {
+
+    double velocidadInicialX();
+
+    double velocidadInicialY();
+
+    double tiempoVueloY();
+
+    double tiempoAlturaMaximaY();
+
+    double alturaMaximaY();
+
+    double posicionFinalX();
+}
